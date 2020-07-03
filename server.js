a29888bdc10f9bfa9704cd9136f3982db6766e97
Susan Holland <segh@fastmail.com>
2020-07-03 13:31:23 -0400

    Added express route to index.html

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// Routes
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
 // Server listener
 app.listen(PORT, function() {
     console.log("Server is listening on: http://localhost:" + PORT);
