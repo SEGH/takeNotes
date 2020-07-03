4e982290e279cd91b8f99131f1ce417f96b7a93d
Susan Holland <segh@fastmail.com>
2020-07-03 13:34:02 -0400

    Added express route to notes.html

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
 
+app.get("/notes", function(req, res) {
+    res.sendFile(__dirname + "/public/notes.html");
+});
+
 // Server listener
 app.listen(PORT, function() {
     console.log("Server is listening on: http://localhost:" + PORT);
