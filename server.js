b9608d4835b8b0ce398f770cdef7c7c8aa1efb1b
Susan Holland <segh@fastmail.com>
2020-07-05 15:24:02 -0400

    API GET Route send db.json

--- server.js
+++ server.js
@@ -3,13 +3,23 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-// Routes
+// const data = [{title: "Stuff to jot down", text: "Thought about things", id: "0"}];
+
+// Serve static assets
+app.use(express.static('public'));
+
+// HTML Routes
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
 
 app.get("/notes", function(req, res) {
     res.sendFile(__dirname + "/public/notes.html");
+});
+
+// API Routes
+app.get("/api/notes", function(req, res) {
+    res.sendFile(__dirname + "/db/db.json");
 });
 
 // Server listener
