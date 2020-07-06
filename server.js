ecdec3f115def974d8d8c6ae19318c29dd6674df
Susan Holland <segh@fastmail.com>
2020-07-06 17:21:53 -0400

    Updated HTML route * to index

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // HTML Routes
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/public/index.html");
-});
-
 app.get("/notes", function (req, res) {
     res.sendFile(__dirname + "/public/notes.html");
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 // API Routes
