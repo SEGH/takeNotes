0fff9cbb1de696e5241b195e6b9d09e2fef8ca3d
Susan Holland <segh@fastmail.com>
2020-07-06 17:44:49 -0400

    Made PORT dynamic

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const fs = require("fs");
 
 // Body Parsing Middleware
